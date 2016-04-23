b3b78bc10fbe593f0a6445562be0df43ada79c56
yamated <tetsuo1208@gmail.com>
2016-04-23 17:01:34 +0900

    :tada: first commit

--- adobecatalyst_read.php
+++ adobecatalyst_read.php
@@ -5,13 +5,14 @@
  * Date: 2016/04/23
  * Time: 16:38
  */
-$filepath="/root/work/userdatasample.csv";
+filepath == "/root/work/userdatasample.csv";
 $file = new SplFileObject($filepath);
 $file->setFlags(SplFileObject::READ_CSV);
 foreach ($file as $line) {
     //終端の空行を除く処理　空行の場合に取れる値は後述
     if(is_null($line[0]){
-    $records[] = $line});
+    $records[] = $line;
+ }
 }
 
 var_dump($records);
